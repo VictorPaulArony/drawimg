1b0c1a2e6b01791d32ff0dca1f01635a60c4cb3e
philip <oumaphilip01@gmail.com>
2025-04-07 09:40:32 +0300

    add drawable and displayable traits

--- drawing/src/main.rs
+++ drawing/src/main.rs
@@ -11,20 +11,14 @@ fn main() {
 
     gs::Point::random(image.width, image.height).draw(&mut image);
 
+    let rectangle = gs::Rectangle::new(&gs::Point::new(150, 150), &gs::Point::new(50, 50));
+    rectangle.draw(&mut image);
 
-    // let rectangle = gs::Rectangle::new(&gs::Point::new(150, 150), &gs::Point::new(50, 50));
-    // rectangle.draw(&mut image);
-
-    let binding = gs::Point::new(250, 700);
-    let binding2 = gs::Point::new(500, 700);
-    let binding3 = gs::Point::new(700, 800);
-    
     let triangle = gs::Triangle::new (
-                &binding,
-                &binding2,
-                &binding3,
-            );
-
+            &gs::Point::new(500, 500),
+            &gs::Point::new(250, 700),
+            &gs::Point::new(700, 800),
+    );
     triangle.draw(&mut image);
 
     for _ in 1..50 {
@@ -32,4 +26,12 @@ fn main() {
     }
 
     raster::save(&image, "image.png").unwrap();
+}
+
+impl Displayable for Image {
+    fn display(&mut self, x: i32, y: i32, color: Color) {
+        if x >= 0 && x < self.width && y >= 0 && y < self.height {
+            self.set_pixel(x, y, color).unwrap();
+        }
+    }
 }
